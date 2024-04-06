6f63b7a5b0b0c4801fdfd03152be6c181bd3f991
Alexopus <alexopus.soft@gmail.com>
2024-04-06 13:59:38 +0200

    formatting

--- nodes.py
+++ nodes.py
@@ -42,32 +42,32 @@ class ImageFromDirSelector:
             "hidden": {"unique_id": "UNIQUE_ID"}
         }
 
-    @classmethod  
+    @classmethod
     def IS_CHANGED(self, directory, unique_id, keep_current_selection, selected_image_filename=None, regexp_filter=None):
         if keep_current_selection:
             return ""
         return selected_image_filename
-    
+
     def get_current_image(self, selected_image_name):
         if self.current_image:
             return self.current_image
         return selected_image_name
-    
+
     def requires_new_image(self, current_image, keep_current_selection):
         if not current_image:
             return True
         return not keep_current_selection
-    
+
     def get_files(self, full_path, regexp_filter):
         files = os.listdir(full_path)
         reg = re.compile(regexp_filter)
         if regexp_filter is not None and regexp_filter is not "":
-          result = []
-          for file in files:
-            if re.search(reg, file):
-                result.append(file)
-          if len(result) > 0:
-            return result
+            result = []
+            for file in files:
+                if re.search(reg, file):
+                    result.append(file)
+            if len(result) > 0:
+                return result
         return files
 
     def sample_images(self, directory, unique_id, keep_current_selection=False, selected_image_name=None, regexp_filter=None):
@@ -78,15 +78,15 @@ class ImageFromDirSelector:
         if new_image_required:
             files = self.get_files(full_path, regexp_filter)
             image_files = [file for file in files if file.endswith(('.png', '.jpg', '.jpeg', 'webp'))]
-            self.current_image = random.choice(image_files) 
+            self.current_image = random.choice(image_files)
             new_image = self.current_image
-          
+
         PromptServer.instance.send_sync("nich-image-selected", {"node_id": unique_id, "value": new_image})
 
         if new_image_required or self.current_tensor is None:
-          image = Image.open(os.path.join(full_path, new_image))
-          self.current_tensor = pil_to_tens(image).unsqueeze(0)
-        
+            image = Image.open(os.path.join(full_path, new_image))
+            self.current_tensor = pil_to_tens(image).unsqueeze(0)
+
         return self.current_tensor
 
 NODE_CLASS_MAPPINGS = {
