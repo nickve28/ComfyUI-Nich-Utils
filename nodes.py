daa9ecace0a5d29bc3f3b44c9a3cf04f7f87623d
nickve28 <nickvaneyk@gmail.com>
2024-04-07 21:03:56 +0200

    refactor the list file logic to filter inside the loop

--- nodes.py
+++ nodes.py
@@ -11,21 +11,33 @@ def pil_to_tens(image):
       image = image.convert('RGB')
     return ToTensor()(image).unsqueeze(0).permute(0, 2, 3, 1)
 
-def list_files(full_path, include_subdirectories):
+
+def file_matches_filter(relative_file_path, filter):
+    if not filter:
+        return True
+
+    return re.search(filter, relative_file_path)
+
+
+def list_files(full_path, include_subdirectories, filename_filter_regexp):
     if include_subdirectories:
         files_list = []
         for root, _directories, files in os.walk(full_path):
             for file_name in files:
                 relative_path = os.path.relpath(os.path.join(root, file_name), full_path)
-                files_list.append(relative_path)
-        return files_list   
+                
+                if file_matches_filter(relative_path, filename_filter_regexp):
+                    files_list.append(relative_path) 
+        return files_list
     
-    return os.listdir(full_path)
+    return [relative_file_path for relative_file_path in os.listdir(full_path) if file_matches_filter(relative_file_path, filename_filter_regexp)]
+
 
 class ImageFromDirSelector:
     def __init__(self) -> None:
         self.current_image = None
         self.current_tensor = None
+
 
     CATEGORY = 'Nich/utils'
     RETURN_TYPES = ("IMAGE",)
@@ -51,21 +63,25 @@ class ImageFromDirSelector:
         }
 
     @classmethod
-    def IS_CHANGED(cls, directory, unique_id, keep_current_selection, selected_image_filename=None, regexp_filter=None,include_subdirectories=False):
+    def IS_CHANGED(cls, directory, unique_id, keep_current_selection, selected_image_filename=None, regexp_filter=None, include_subdirectories=False):
         return "" if keep_current_selection else selected_image_filename
+
 
     def get_current_image(self, selected_image_name):
         return selected_image_name if selected_image_name else self.current_image
 
+
     def requires_new_image(self, current_image, keep_current_selection):
         return not current_image or not keep_current_selection
 
+
     def get_files(self, full_path, regexp_filter, include_subdirectories):
-        files = list_files(full_path, include_subdirectories)
+        filename_filter_regexp = None
         if regexp_filter:
-            reg = re.compile(regexp_filter)
-            files = [file for file in files if re.search(reg, file)]
-        return files
+            filename_filter_regexp = re.compile(regexp_filter)
+
+        return list_files(full_path, include_subdirectories, filename_filter_regexp)
+
 
     def sample_images(self, directory, unique_id, keep_current_selection=False, selected_image_name=None, regexp_filter=None,include_subdirectories=False):
         full_path = os.path.expanduser(directory)
@@ -86,7 +102,7 @@ class ImageFromDirSelector:
 
         return self.current_tensor
 
+
 NODE_CLASS_MAPPINGS = {
     "Image from Dir Selector (Nich)": ImageFromDirSelector
 }
-
