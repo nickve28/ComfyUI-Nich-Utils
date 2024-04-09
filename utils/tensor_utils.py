36d3e9c5da1216d7956c98aa9933d2e2739ff04d
Nick van Eijk <nickve28@users.noreply.github.com>
2024-04-09 07:19:37 +0200

    Update tensor_utils.py
    Use consistent indentation

--- utils/tensor_utils.py
+++ utils/tensor_utils.py
@@ -9,5 +9,5 @@ if the given image is a RGBA image (has alpha channel), it is converted to RGB p
 """
 def pil_to_tens(image: Image) -> Tensor:
     if image.mode == 'RGBA':
-      image = image.convert('RGB')
+        image = image.convert('RGB')
     return ToTensor()(image).unsqueeze(0).permute(0, 2, 3, 1)
